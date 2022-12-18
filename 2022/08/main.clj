2d72032cc7097b0b92dbcc8268b93c3bda45e1a3
Ngoc Khuat <qn.khuat@gmail.com>
2022-12-18 16:11:26 +0700

    08 part 2

--- 2022/08/main.clj
+++ 2022/08/main.clj
@@ -1,36 +1,4 @@
 (require '[clojure.string :as str])
-
-(def input
-  (->> (slurp "input.txt")
-       str/split-lines
-       (map (fn [row] (map #(Integer/parseInt %) (str/split row #""))))))
-
-(defn two-d-array
-  [nested-lists]
-  (let [col-count (count (first nested-lists))
-        row-count (count nested-lists)
-        values    (apply concat nested-lists)
-        get-value (fn [row col]
-                    (when (and (< row row-count)
-                               (< col col-count))
-                      (nth values (+ col (* row row-count)))))]
-    {:get-value get-value
-     :get-row   (fn [row]
-                  (->> values
-                       (drop (* col-count row))
-                       (take col-count)))
-     :get-col   (fn [col]
-                  (for [r (range 0 row-count)]
-                    (get-value r col)))
-     :row-count row-count
-     :col-count col-count}))
-
-(every? #(< % 4) [1 2 3])
-
-(defn one-side-visible?
-  [side height]
-  (every? #(< % height) side))
-
 #_(def input
     [[3 0 3 7 3]
      [2 5 5 1 2]
@@ -51,8 +19,7 @@
     (one-side-visible? (drop (inc row-index) col) height)))
 
 ;; part 1
-(let [{:keys [get-value get-row get-col row-count col-count]}
-      (two-d-array input)
+(let [{:keys [get-value get-row get-col row-count col-count]} (two-d-array input)
       visible-trees (atom (+ row-count row-count col-count col-count -4))]
   (doseq [r (range 1 (dec row-count))
           c (range 1 (dec col-count))
@@ -62,3 +29,31 @@
 ;; it runs for a long time
 ;; => 1820
 
+(defn viewing-distance
+  [trees view-height]
+  trees
+  view-height
+  (reduce (fn [cnt tree-height]
+            (if (>= tree-height view-height)
+              (reduced (inc cnt))
+              (inc cnt)))
+          0 trees))
+
+(defn scenic-score
+  [row-index col-index row col height]
+  (apply *
+         [;; from the left
+          (viewing-distance (reverse (take col-index row)) height)
+          ;; from the right
+          (viewing-distance (drop (inc col-index) row) height)
+          ;; from the top
+          (viewing-distance (reverse (take row-index col)) height)
+          ;; from bottom
+          (viewing-distance (drop (inc row-index) col) height)]))
+
+;; part 2
+(let [{:keys [get-value get-row get-col row-count col-count]} (two-d-array input)]
+ (apply max (for [r (range 0 row-count)
+                  c (range 0 col-count)]
+              (scenic-score r c (get-row r) (get-col c) (get-value r c)))))
+;; => 385112
