6f9b11ce26cc881db0a1663c0bdc3ae3404f5c3b
Ngoc Khuat <qn.khuat@gmail.com>
2022-12-23 10:07:42 +0700

    09 part 2 unsucessful

--- 2022/09_main.clj
+++ 2022/09_main.clj
@@ -66,17 +66,6 @@
       (let [new-tail-pos (maybe-move-tail tail head)]
         (recur new-tail-pos head (conj tail-pos-walked new-tail-pos))))))
 
-#_(def steps
-    [[:r 4]
-     [:u 4]
-     [:l 3]
-     [:d 1]
-     [:r 4]
-     [:d 1]
-     [:l 5]
-     [:r 2]])
-
-
 ;; part 1
 (def result (reduce
               (fn [{:keys [tail head tail-pos-walked]} [direction step :as _step]]
@@ -85,7 +74,6 @@
                   {:tail tail
                    :head head
                    :tail-pos-walked (set/union tail-pos-walked tail-walked)}))
-
               {:tail [0 0]
                :head [0 0]
                :tail-pos-walked #{[0 0]}}
@@ -94,3 +82,46 @@
     :tail-pos-walked
     count)
 ;; => 5513
+
+;; part 2
+
+(defn move-tail-to-meet-head
+  [tail head]
+  (reduce
+    (fn [{:keys [tail head tail-pos-walked]} [direction step :as _step]]
+      (let [[tail head tail-walked]
+            (move-til-touch tail (move head direction step))]
+        {:tail tail
+         :head head
+         :tail-pos-walked (set/union tail-pos-walked tail-walked)}))
+    {:tail            tail
+     :head            head
+     :tail-pos-walked #{[0 0]}}
+    steps))
+
+;; part 1
+(def result-2
+  (reduce
+    (fn [{:keys [rope tail-pos-walked]} [direction step :as _step]]
+      (let [[head & tails] rope
+            new-head (move head direction step)
+            new-rope (concat [new-head] tails)
+            pairs    (partition 2 1 new-rope)]
+        (reduce (fn [{:keys [rope]} [head tail]]
+                  (let [[tail _head sub-tail-pos-walked] (move-til-touch tail head)]
+                    {:rope            (conj rope tail)
+                     ;; just do a replacement because we only care about the last anyway
+                     :tail-pos-walked (set/union tail-pos-walked sub-tail-pos-walked)}))
+                {:rope            [new-head]
+                 :tail-pos-walked #{}}
+                pairs)))
+    {:rope            (repeat 10 [0 0]) ;; head is first
+     :tail-pos-walked #{[0 0]}}
+    steps))
+
+(-> result-2
+    :tail-pos-walked
+    count)
+;; => 2364
+
+;; right answer 2427
